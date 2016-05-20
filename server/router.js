fd85863b8a8b2dd38bf2aa296b032e65ead8b788
Cristian Drincu <cristidrincu@gmail.com>
2016-05-20 19:09:36 +0300

    Refactored method name

--- server/router.js
+++ server/router.js
@@ -7,14 +7,14 @@ const passport = require('passport');
 
 //middleware for authentication on the routes we need
 const requireAuth = passport.authenticate('jwt', { session: false }); //no session since we do not use cookies
-const requireSigning = passport.authenticate('local', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
     app.get('/', requireAuth, function(req, res) {
         res.status(200).send({ hi: 'there' });
     });
 
-    app.post('/signin', requireSigning, Authentication.signin);
+    app.post('/signin', requireSignin, Authentication.signin);
 
     app.post('/signup', Authentication.signup);
 };
